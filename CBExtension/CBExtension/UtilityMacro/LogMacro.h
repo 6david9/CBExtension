2c33c00a31f2a0bad6517714d94e466a8220d383
ly <6david9@163.com>
2013-11-29 14:09:04 +0800

    修改日志输出策略

--- CBExtension/CBExtension/UtilityMacro/LogMacro.h
+++ CBExtension/CBExtension/UtilityMacro/LogMacro.h
@@ -9,26 +9,17 @@
 #ifndef LogMacro_h
 #define LogMacro_h
 
+
 #ifdef DEBUG
-
 #define CBLog(format, ...)   NSLog(format, ##__VA_ARGS__)
-#define po(obj)              NSLog(@"%@", obj)
-#define pi(var_i)            NSLog(@"%d", var_i)
-#define pf(var_f)            NSLog(@"%f", var_f)
-
-#define print_function()     NSLog(@"%s", __PRETTY_FUNCTION__)
-
 #else
-
 #define CBLog(format, ...)  do{ }while(0)
-#define po(obj)             do{ }while(0)
-#define pi(var_i)           do{ }while(0)
-#define pf(var_f)           do{ }while(0)
-
-#define print_function()    do{ }while(0)
-
 #endif
 
+#define po(obj)              CBLog(@"%@", obj)
+#define pi(var_i)            CBLog(@"%d", var_i)
+#define pf(var_f)            CBLog(@"%f", var_f)
+#define print_function()     CBLog(@"%s", __PRETTY_FUNCTION__)
 
 
 #endif
